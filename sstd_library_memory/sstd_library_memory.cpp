1fb3ca17f22b79e81f8ab70ab8cb2f02c83f4cfa
ngzHappy <zhaixueqiang@hotmail.com>
2019-03-17 04:23:28 +0800

    +test code

--- sstd_library_memory/sstd_library_memory.cpp
+++ sstd_library_memory/sstd_library_memory.cpp
@@ -1,16 +1,66 @@
 ﻿#include "sstd_library_memory.hpp"
 
+namespace _theSSTDLibraryMemoryFile {
+
+    inline void * auto_malloc(std::size_t n){
+        return ::operator new(n);
+    }
+
+     inline void auto_free(void * ptr){
+         ::operator delete(ptr);
+     }
+
+     inline void * auto_malloc(std::size_t c, std::align_val_t a){
+         return ::operator new(c, a);
+     }
+
+     inline void auto_free(void* ptr, std::align_val_t al){
+        return ::operator delete(ptr, al);
+     }
+
+}/*namespace _theSSTDLibraryMemoryFile*/
+
 namespace sstd {
+
+    void* StaticMemoryObject::operator new (std::size_t count){
+        return _theSSTDLibraryMemoryFile::auto_malloc(count);
+    }
+
+    void StaticMemoryObject::operator delete(void * ptr){
+        return _theSSTDLibraryMemoryFile::auto_free(ptr);
+    }
+
+    void* StaticMemoryObject::operator new[](std::size_t count){
+        return _theSSTDLibraryMemoryFile::auto_malloc(count);
+    }
+
+    void StaticMemoryObject::operator delete[](void * ptr){
+        return _theSSTDLibraryMemoryFile::auto_free(ptr);
+    }
+
+    void* StaticMemoryObject::operator new(std::size_t count, std::align_val_t al){
+        return _theSSTDLibraryMemoryFile::auto_malloc(count,al);
+    }
+
+    void StaticMemoryObject::operator delete(void* ptr, std::align_val_t al){
+        return _theSSTDLibraryMemoryFile::auto_free(ptr,al);
+    }
+
+    void* StaticMemoryObject::operator new[](std::size_t count, std::align_val_t al){
+        return _theSSTDLibraryMemoryFile::auto_malloc(count,al);
+    }
+
+    void StaticMemoryObject::operator delete[](void* ptr, std::align_val_t al){
+        return _theSSTDLibraryMemoryFile::auto_free(ptr,al);
+    }
+
+}/*namespace sstd*/
+
 
 /*
 #pragma pack(push, 2)
 #pragma pack(pop)
 */
-
-}/*namespace sstd*/
-
-
-
 
 
 
--- sstd_library_memory/sstd_library_memory.hpp
+++ sstd_library_memory/sstd_library_memory.hpp
@@ -6,6 +6,10 @@
 #include <memory>
 #include <utility>
 
+#if __has_include(<memory_resource>)
+#include <memory_resource>
+#endif
+
 namespace _theSSTDLibraryMemoryFile {
 }/*namespace _theSSTDLibraryMemoryFile*/
 
@@ -15,6 +19,30 @@ namespace sstd {
         sstd_default_copy_create(StaticMemoryObject);
         StaticMemoryObject()=default;
         ~StaticMemoryObject()=default;
+    public:
+        static void* operator new (std::size_t count);
+        static void operator delete(void * ptr);
+
+        static void* operator new[](std::size_t count);
+        static void operator delete[](void * ptr);
+
+        static void* operator new(std::size_t count, std::align_val_t al);
+        static void operator delete(void* ptr, std::align_val_t al);
+
+        static void* operator new[](std::size_t count, std::align_val_t al);
+        static void operator delete[](void* ptr, std::align_val_t al);
+
+        static inline void* operator new  (std::size_t, void* ptr) noexcept {
+            return ptr;
+        }
+        static inline void* operator new[](std::size_t, void* ptr) noexcept {
+            return ptr;
+        }
+
+        static constexpr inline void operator delete  (void *, void *) noexcept {
+        }
+        static constexpr inline void operator delete[](void *, void *) noexcept {
+        }
     };
 }/*namespace sstd*/
 
