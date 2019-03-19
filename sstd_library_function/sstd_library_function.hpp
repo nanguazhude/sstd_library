0baceaadfa851a61796dbf237d5f05874e97163c
ngzHappy <zhaixueqiang@hotmail.com>
2019-03-19 23:35:43 +0800

    Update sstd_library_function.hpp

--- sstd_library_function/sstd_library_function.hpp
+++ sstd_library_function/sstd_library_function.hpp
@@ -2,17 +2,17 @@
 
 #include "../sstd_library.hpp"
 
-namespace sstd{
+namespace sstd {
 
     /*T should have ->start() noexcept ; */
     template <typename T>
-    class StartFunction{
+    class StartFunction {
         T thisFunction;
     public:
 
         template<typename U,
-                 typename = std::enable_if_t< std::is_constructible_v< T, U&& > > >
-        inline StartFunction(U && arg) :
+            typename = std::enable_if_t< std::is_constructible_v< T, U&& > > >
+            inline StartFunction(U && arg) :
             thisFunction{ std::forward<U>(arg) } {
         }
 
@@ -30,24 +30,28 @@ namespace sstd{
         sstd_class(StartFunction);
     };
 
-    template <typename U,typename T,typename E>
-    class BindDataWithFunction{
+    template <typename U, typename T, typename E, typename N>
+    class BindDataWithFunction {
         U other;
         T fun;
         E exceptionFun;
+        N normalFun;
     public:
-        template<typename A0,typename A1,typename A2>
-        inline BindDataWithFunction(A0&&argOther,A1 && argFun,A2 && argEFun) :
+        template<typename A0, typename A1, typename A2, typename A3>
+        inline BindDataWithFunction(A0&&argOther, A1 && argFun, A2 && argEFun, A3 && argNFun) :
             other(std::forward<A0>(argOther)),
-            fun(std::forward<A1>(argFun)) ,
-            exceptionFun(std::forward<A2>(argEFun)){
+            fun(std::forward<A1>(argFun)),
+            exceptionFun(std::forward<A2>(argEFun)),
+            normalFun(std::forward<A3>(argNFun)) {
         }
         inline void operator()() const noexcept {
             sstd_try{
                 (const_cast<BindDataWithFunction *>(this)->fun)();
-            }sstd_catch(...){
+            }sstd_catch(...) {
                 exceptionFun();
+                return;
             }
+            normalFun();
         }
     public:
         sstd_default_copy_create(BindDataWithFunction);
@@ -55,8 +59,8 @@ namespace sstd{
         sstd_class(BindDataWithFunction);
     };
 
-    template <typename U,typename ... Args>
-    inline StartFunction< std::shared_ptr<U> > makeStartFunction( Args && ... args ){
+    template <typename U, typename ... Args>
+    inline StartFunction< std::shared_ptr<U> > makeStartFunction(Args && ... args) {
         return StartFunction< std::shared_ptr<U> >{
             sstd_make_shared<U>(std::forward<Args>(args)...)
         };
@@ -64,20 +68,20 @@ namespace sstd{
 
 }/*namespace sstd*/
 
-template <typename U,typename ... Args>
-inline sstd::StartFunction< std::shared_ptr<U> > sstd_make_start_function( Args && ... args ){
+template <typename U, typename ... Args>
+inline sstd::StartFunction< std::shared_ptr<U> > sstd_make_start_function(Args && ... args) {
     return sstd::makeStartFunction<U>(std::forward<Args>(args)...);
 }
 
-namespace sstd{
-    class SSTD_SYMBOL_DECL YieldFunctionBasic{
+namespace sstd {
+    class SSTD_SYMBOL_DECL YieldFunctionBasic {
         sstd_delete_copy_create(YieldFunctionBasic);
     public:
         YieldFunctionBasic();
         virtual ~YieldFunctionBasic();
     protected:
         virtual void doRun() = 0;
-        virtual void doException() noexcept ;
+        virtual void doException() noexcept;
     private:
         sstd_class(YieldFunctionBasic);
     };
@@ -94,36 +98,41 @@ namespace sstd {
         YieldResumeFunctionPrivate * const thisPrivate;
         sstd_delete_copy_create(YieldResumeFunction);
         using shared_super = std::enable_shared_from_this<YieldResumeFunction>;
-        class InAnotherFunctionStatck{
+
+        template<bool>
+        class InAnotherFunctionStatck {
             YieldResumeFunction * super;
         public:
             sstd_default_copy_create(InAnotherFunctionStatck);
         public:
-            inline InAnotherFunctionStatck(YieldResumeFunction *);
+            inline InAnotherFunctionStatck(YieldResumeFunction * arg) : super(arg) {
+            }
             inline void operator()() const noexcept;
         };
+
     public:
-        YieldResumeFunction(std::size_t=1024uLL*1024uLL*64uLL);
+        YieldResumeFunction(std::size_t = 1024uLL * 1024uLL * 64uLL);
         virtual ~YieldResumeFunction();
     public:
         void start() noexcept;
         template<typename T>
         using BindDataFunction = BindDataWithFunction< std::shared_ptr<const void>,
-        std::remove_cv_t< std::remove_reference_t<T> > ,
-        InAnotherFunctionStatck>;
+            std::remove_cv_t< std::remove_reference_t<T> >,
+            InAnotherFunctionStatck<true>,
+            InAnotherFunctionStatck<false>>;
     protected:
         template<typename T>
         inline BindDataFunction<T> bindFunctionWithThis(T &&) const noexcept;
     private:
-        void yield() noexcept ;
-        void resume() noexcept ;
+        void yield() noexcept;
+        void resume() noexcept;
         bool hasException() const noexcept;
     protected:
         void directRun() noexcept;
     private:
-        void resumeWithException() noexcept ;
-        void directYield() noexcept ;
-        void directResume() noexcept ;
+        void resumeWithException() noexcept;
+        void directYield() noexcept;
+        void directResume() noexcept;
     private:
         using shared_super::shared_from_this;
         using shared_super::weak_from_this;
@@ -136,16 +145,18 @@ namespace sstd {
     template<typename T>
     inline YieldResumeFunction::BindDataFunction<T> YieldResumeFunction::bindFunctionWithThis(T && arg) const noexcept {
         return { const_cast<YieldResumeFunction*>(this)->copyThisToAnotherStack() ,
-                    std::forward<T>(arg) ,
-                    InAnotherFunctionStatck{const_cast<YieldResumeFunction*>(this)} };
+                 std::forward<T>(arg) ,
+                 InAnotherFunctionStatck<true>{const_cast<YieldResumeFunction*>(this)},
+                 InAnotherFunctionStatck<false>{const_cast<YieldResumeFunction*>(this)} };
     }
 
-    inline YieldResumeFunction::InAnotherFunctionStatck::InAnotherFunctionStatck(YieldResumeFunction *arg) :
-        super(arg){
-    }
-
-    inline void YieldResumeFunction::InAnotherFunctionStatck::operator()() const noexcept{
-        super->resumeWithException();
+    template<bool V>
+    inline void YieldResumeFunction::InAnotherFunctionStatck<V>::operator()() const noexcept {
+        if constexpr (V) {
+            super->resumeWithException();
+        } else {
+            super->resume();
+        }
     }
 
     class _YieldResumeFunctionPrivate final {
@@ -156,9 +167,6 @@ namespace sstd {
         }
         inline bool hasException() const {
             return super->hasException();
-        }
-        inline void resume() const {
-            super->resume();
         }
     };
 
@@ -172,11 +180,6 @@ namespace sstd {
     } static_assert(true)
 #endif
 
-#ifndef sstd_function_resume
-#define sstd_function_resume(...) \
-    ::sstd::_YieldResumeFunctionPrivate{this}.resume(); \
-    static_assert(true)
-#endif
 
 
 
