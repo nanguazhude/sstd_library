44654548738fee4920e7e34f231511160ca188d4
ngzHappy <zhaixueqiang@hotmail.com>
2019-04-06 14:50:22 +0800

    +constexpr_float

--- sstd_library_constexpr/float_constexpr.hpp
+++ sstd_library_constexpr/float_constexpr.hpp
@@ -1,8 +1,31 @@
 ﻿
 #pragma once
 
+#include <ratio>
+#include <limits>
 
+namespace sstd {
 
+    template<intmax_t Num_,intmax_t Den_>
+    class constexpr_float : public std::ratio<Num_,Den_> {
+        using super = std::ratio<Num_,Den_>;
+    public:
+        template<typename T1 = double >
+        static inline constexpr auto toFloat() noexcept {
+            using T = std::remove_cv_t< std::remove_reference_t< T1 > >;
+            if constexpr ( super::den == 1  ){
+                return static_cast< T >(super::num);
+            } else if constexpr( super::den == 0 ){
+                return std::numeric_limits< T >::quiet_NaN() ;
+            } else {
+                using CastType = long double;
+                return static_cast<T>( static_cast<CastType>( super::num ) /
+                                       static_cast< CastType >(super::den) );
+            }
+        }
+    };
+
+}/*namespace sstd*/
 
 
 
