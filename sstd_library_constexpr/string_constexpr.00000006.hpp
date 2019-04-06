88fe805637f6ac503636c2fb191ff3e4a1f4cc25
ngzHappy <zhaixueqiang@hotmail.com>
2019-04-06 14:29:10 +0800

    +update constexpr string

---
+++ sstd_library_constexpr/string_constexpr.00000006.hpp
@@ -0,0 +1,6 @@
+ template< auto A0 
+,auto A1 ,auto A2 ,auto A3 ,auto A4 ,auto A5 ,auto A6  ,auto ... Args >  class string_constexpr_builder_helper< 7, A0
+ , A1  , A2  , A3  , A4  , A5  , A6  ,Args ... > {
+public: 
+using type = constexpr_string_literal< A0   , A1  , A2  , A3  , A4  , A5  , A6 >;
+};
