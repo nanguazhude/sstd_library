4331bae913f01fb7f6b0e3f81d5a94b014604e6d
ngzHappy <zhaixueqiang@hotmail.com>
2019-04-06 18:08:08 +0800

    +测试代码

--- sstd_library_constexpr/float_constexpr.hpp
+++ sstd_library_constexpr/float_constexpr.hpp
@@ -8,23 +8,7 @@ namespace sstd {
 
     namespace detail{
         template<intmax_t Num_,intmax_t Den_ >
-        class constexpr_float : public std::ratio<Num_,Den_> {
-            using super = std::ratio<Num_,Den_>;
-        public:
-            template<typename T1 = double >
-            static inline constexpr auto toFloat() noexcept {
-                using T = std::remove_cv_t< std::remove_reference_t< T1 > >;
-                if constexpr ( super::den == 1  ){
-                    return static_cast< T >(super::num);
-                } else if constexpr( super::den == 0 ){
-                    return std::numeric_limits< T >::quiet_NaN() ;
-                } else {
-                    using CastType = long double;
-                    return static_cast<T>( static_cast< CastType >( super::num ) /
-                                           static_cast< CastType >( super::den) );
-                }
-            }
-        };
+        using constexpr_float = std::ratio<Num_, Den_>;
     }
 
     template<intmax_t Num_,intmax_t Den_=1>
@@ -35,6 +19,24 @@ namespace sstd {
 
     using constexpr_float_exp = constexpr_float< 27'182'818'284'590'452,
                                                  10'000'000'000'000'000>;
+
+    using constexpr_float_one = constexpr_float< 1, 1>;
+
+    template<typename R,typename T1 = double>
+    inline constexpr auto toFloat(){
+        using super = std::remove_cv_t < std::remove_reference_t < R > >;
+        using T = std::remove_cv_t< std::remove_reference_t< T1 > >;
+        if constexpr (super::den == 1) {
+            return static_cast<T>(super::num);
+        } else if constexpr (super::den == 0) {
+            return std::numeric_limits< T >::quiet_NaN();
+        } else {
+            using CastType = long double;
+            return static_cast<T>(static_cast<CastType>(super::num) /
+                static_cast<CastType>(super::den));
+        }
+
+    }
 
 }/*namespace sstd*/
 
--- sstd_library_test/constexpr_test.cpp
+++ sstd_library_test/constexpr_test.cpp
@@ -1,32 +1,40 @@
 ﻿
 #include <sstd_library.hpp>
 
-inline auto * data1(){
-    return sstd::constexpr_array<1,2,3,4>::data();
+inline auto * data1() {
+    return sstd::constexpr_array<1, 2, 3, 4>::data();
 }
 
-inline auto * data2(){
-    return sstd::constexpr_array<1,2,3,4>::data();
+inline auto * data2() {
+    return sstd::constexpr_array<1, 2, 3, 4>::data();
 }
 
-inline auto * data3(){
+inline auto * data3() {
     return sstd_cstr("aabbccdd")::toStringView().data();
 }
 
-inline auto * data4(){
+inline auto * data4() {
     return sstd_cstr("aabbccdd")::toStringView().data();
 }
 
-extern void constexpr_test(){
+extern void constexpr_test() {
 
-    assert( data1() == data2() );
-    assert( data3() == data4() );
-    static_assert ( sstd::constexpr_array<1,2,3,4>::size() == 4 );
+    assert(data1() == data2());
+    assert(data3() == data4());
+    static_assert (sstd::constexpr_array<1, 2, 3, 4>::size() == 4);
     std::cout << sstd_cstr("aabbccdd")::toConstexprStringView().size() << std::endl;
 
-    assert( sstd_cstr(u8R"(今天)")::toStringView() == u8R"(今天)"sv);
-    assert( sstd_cstr(u8R"(今天)")::toConstexprStringView() == u8R"(今天)"sv);
-    assert( sstd_cstr(u8R"(今天)")::toConstexprStringView().data() == sstd_cstr(u8R"(今天)")::toStringView().data() );
+    assert(sstd_cstr(u8R"(今天)")::toStringView() == u8R"(今天)"sv);
+    assert(sstd_cstr(u8R"(今天)")::toConstexprStringView() == u8R"(今天)"sv);
+    assert(sstd_cstr(u8R"(今天)")::toConstexprStringView().data() == sstd_cstr(u8R"(今天)")::toStringView().data());
+
+    assert(std::abs((1.0 / 3.141592654) -
+        sstd::toFloat<std::ratio_divide<
+        sstd::constexpr_float_one, sstd::constexpr_float_pi >>()) < 0.001);
+
+    static_assert((sstd::toFloat< sstd::constexpr_float_pi >() - 3.141592654) < 0.001);
+    static_assert((3.141592654 - sstd::toFloat< sstd::constexpr_float_pi >()) < 0.001);
+
 }
 
 
