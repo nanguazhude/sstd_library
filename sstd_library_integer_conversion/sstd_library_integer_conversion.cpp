76b458b0727045a777fc657f43487cbb27cb93c2
ngzHappy <zhaixueqiang@hotmail.com>
2019-04-11 20:46:36 +0800

    +tonumber

--- sstd_library_integer_conversion/sstd_library_integer_conversion.cpp
+++ sstd_library_integer_conversion/sstd_library_integer_conversion.cpp
@@ -12,13 +12,28 @@ namespace sstd {
 
     namespace detail {
 
+        using string_type_ = std::basic_string<char, std::char_traits<char>, sstd::allocator<char>>;
+
 #if HAS_INCLUDE_SSTD_CHARCONV_
+
         template<typename T>
         inline static T \uacf1ToNumber(const std::string_view & arg) {
             T varAns;
             std::from_chars(arg.data(), arg.data() + arg.size(), varAns);
             return varAns;
         }
+
+        template<typename T>
+        inline static string_type_ \uacf1ToString(const T & arg) {
+            string_type_ varAns{ 64,char(0) };
+            auto[p, e] = std::to_chars(varAns.data(), varAns.data() + varAns.size(), arg);
+            if (e != std::errc{}) {
+                return{};
+            }
+            varAns.resize(static_cast<std::size_t>(p - varAns.data()));
+            return std::move(varAns);
+        }
+
 #else
         template<typename T>
         inline static T \uacf1ToNumber(const std::string_view & arg) {
@@ -68,6 +83,66 @@ namespace sstd {
 
         SSTD_SYMBOL_DECL char16_t toChar16_t(std::string_view arg) {
             return \uacf1ToNumber<std::uint16_t>(arg);
+        }
+
+        SSTD_SYMBOL_DECL wchar_t toWChar_t(std::string_view arg) {
+            if constexpr (sizeof(wchar_t) == 1) {
+                return (unsigned char)(toUnsignedChar(arg));
+            } else if constexpr (sizeof(wchar_t) == 2) {
+                return (wchar_t)(toChar16_t(arg));
+            } else {
+                return (wchar_t)(toChar32_t(arg));
+            }
+        }
+
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringWChar_t(wchar_t arg) {
+            if constexpr (sizeof(wchar_t) == 1) {
+                return fromStringChar((char)(arg));
+            } else if constexpr (sizeof(wchar_t) == 2) {
+                return fromStringChar16_t((char16_t)(arg));
+            } else {
+                return fromStringChar32_t((char32_t)(arg));
+            }
+        }
+
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringInt(int arg) {
+            return \uacf1ToString(arg);
+        }
+
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringUnsignedInt(unsigned int arg) {
+            return \uacf1ToString(arg);
+        }
+
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringShort(short arg) {
+            return \uacf1ToString(arg);
+        }
+
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringUnsignedShort(unsigned short arg) {
+            return \uacf1ToString(arg);
+        }
+
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringLong(long arg) {
+            return \uacf1ToString(arg);
+        }
+
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringUnsignedLong(unsigned long arg) {
+            return \uacf1ToString(arg);
+        }
+
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringLongLong(long long arg) {
+            return \uacf1ToString(arg);
+        }
+
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringUnsignedLongLong(unsigned long long arg) {
+            return \uacf1ToString(arg);
+        }
+
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringChar32_t(char32_t arg) {
+            return \uacf1ToString(*reinterpret_cast<std::uint32_t*>(&arg));
+        }
+
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringChar16_t(char16_t arg) {
+            return \uacf1ToString(*reinterpret_cast<std::uint16_t*>(&arg));
         }
 
     }/*namespace detail*/
--- sstd_library_integer_conversion/sstd_library_integer_conversion.hpp
+++ sstd_library_integer_conversion/sstd_library_integer_conversion.hpp
@@ -21,8 +21,9 @@ namespace sstd{
         SSTD_SYMBOL_DECL long long toLongLong(std::string_view);
         SSTD_SYMBOL_DECL unsigned long long toUnsignedLongLong(std::string_view);
 
-        SSTD_SYMBOL_DECL char32_t toChar32_t(std::string_view arg);
-        SSTD_SYMBOL_DECL char16_t toChar16_t(std::string_view arg);
+        SSTD_SYMBOL_DECL char32_t toChar32_t(std::string_view);
+        SSTD_SYMBOL_DECL char16_t toChar16_t(std::string_view);
+        SSTD_SYMBOL_DECL wchar_t toWChar_t(std::string_view);
 
         inline char toChar(std::string_view arg) {
             if (arg.empty()) {
@@ -61,9 +62,27 @@ namespace sstd{
             return{ 1,(char)(arg) };
         }
 
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringInt(int);
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringUnsignedInt(unsigned int);
 
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringShort(short);
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringUnsignedShort(unsigned short);
+
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringLong(long);
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringUnsignedLong(unsigned long );
+
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringLongLong(long long);
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringUnsignedLongLong(unsigned long long);
+
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringChar32_t(char32_t);
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringChar16_t(char16_t);
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringWChar_t(wchar_t);
 
     }/*namespace detail*/
+
+    inline void toNumber(std::string_view arg,int & ans) {
+        ans = detail::toInt(arg);
+    }
 
 }/*namespace sstd*/
 
