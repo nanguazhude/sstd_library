4fa719d3740bc0f2f717b17934d3f596bcbba2fd
ngzHappy <zhaixueqiang@hotmail.com>
2019-04-11 20:13:38 +0800

    Update sstd_library_integer_conversion.cpp

--- sstd_library_integer_conversion/sstd_library_integer_conversion.cpp
+++ sstd_library_integer_conversion/sstd_library_integer_conversion.cpp
@@ -14,14 +14,14 @@ namespace sstd {
 
 #if HAS_INCLUDE_SSTD_CHARCONV_
         template<typename T>
-        inline static T toNumber(const std::string_view & arg) {
+        inline static T \uacf1ToNumber(const std::string_view & arg) {
             T varAns;
             std::from_chars(arg.data(), arg.data() + arg.size(), varAns);
             return varAns;
         }
 #else
         template<typename T>
-        inline static T toNumber(const std::string_view & arg) {
+        inline static T \uacf1ToNumber(const std::string_view & arg) {
             std::stringstream varConv;
             T varAns;
             varConv << arg;
@@ -31,35 +31,35 @@ namespace sstd {
 #endif
 
         SSTD_SYMBOL_DECL int toInt(std::string_view arg) {
-            return toNumber<int>(arg);
+            return \uacf1ToNumber<int>(arg);
         }
 
         SSTD_SYMBOL_DECL unsigned int toUnsignedInt(std::string_view arg) {
-            return toNumber<unsigned int>(arg);
+            return \uacf1ToNumber<unsigned int>(arg);
         }
 
         SSTD_SYMBOL_DECL long toLong(std::string_view arg) {
-            return toNumber<long>(arg);
+            return \uacf1ToNumber<long>(arg);
         }
 
         SSTD_SYMBOL_DECL unsigned long toUnsignedLong(std::string_view arg) {
-            return toNumber<unsigned long>(arg);
+            return \uacf1ToNumber<unsigned long>(arg);
         }
 
         SSTD_SYMBOL_DECL short toShort(std::string_view arg) {
-            return toNumber<short>(arg);
+            return \uacf1ToNumber<short>(arg);
         }
 
         SSTD_SYMBOL_DECL unsigned short toUnsignedShort(std::string_view arg) {
-            return toNumber<unsigned short>(arg);
+            return \uacf1ToNumber<unsigned short>(arg);
         }
 
         SSTD_SYMBOL_DECL long long toLongLong(std::string_view arg) {
-            return toNumber<long long>(arg);
+            return \uacf1ToNumber<long long>(arg);
         }
 
         SSTD_SYMBOL_DECL unsigned long long toUnsignedLongLong(std::string_view arg) {
-            return toNumber<unsigned long long>(arg);
+            return \uacf1ToNumber<unsigned long long>(arg);
         }
 
     }/*namespace detail*/
