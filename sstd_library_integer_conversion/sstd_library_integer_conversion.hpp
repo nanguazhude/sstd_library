652f02d4512b56b04f3d9e64685c56e573e1c7f0
ngzHappy <zhaixueqiang@hotmail.com>
2019-04-11 21:07:02 +0800

    Update sstd_library_integer_conversion.hpp

--- sstd_library_integer_conversion/sstd_library_integer_conversion.hpp
+++ sstd_library_integer_conversion/sstd_library_integer_conversion.hpp
@@ -5,9 +5,9 @@
 #include "../sstd_library.hpp"
 
 
-namespace sstd{
+namespace sstd {
 
-    namespace detail{
+    namespace detail {
 
         SSTD_SYMBOL_DECL int toInt(std::string_view);
         SSTD_SYMBOL_DECL unsigned int toUnsignedInt(std::string_view);
@@ -51,7 +51,7 @@ namespace sstd{
     namespace detail {
 
         inline std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringChar(char arg) {
-            return{1,arg};
+            return{ 1,arg };
         }
 
         inline std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringSignedChar(signed char arg) {
@@ -69,7 +69,7 @@ namespace sstd{
         SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringUnsignedShort(unsigned short);
 
         SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringLong(long);
-        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringUnsignedLong(unsigned long );
+        SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringUnsignedLong(unsigned long);
 
         SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringLongLong(long long);
         SSTD_SYMBOL_DECL std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> fromStringUnsignedLongLong(unsigned long long);
@@ -80,8 +80,116 @@ namespace sstd{
 
     }/*namespace detail*/
 
-    inline void toNumber(std::string_view arg,int & ans) {
+    inline void toNumber(std::string_view arg, int & ans) {
         ans = detail::toInt(arg);
+    }
+
+    inline void toNumber(std::string_view arg, unsigned int & ans) {
+        ans = detail::toUnsignedInt(arg);
+    }
+
+    inline void toNumber(std::string_view arg, short & ans) {
+        ans = detail::toShort(arg);
+    }
+
+    inline void toNumber(std::string_view arg, unsigned short & ans) {
+        ans = detail::toUnsignedShort(arg);
+    }
+
+    inline void toNumber(std::string_view arg, long & ans) {
+        ans = detail::toLong(arg);
+    }
+
+    inline void toNumber(std::string_view arg, unsigned long & ans) {
+        ans = detail::toUnsignedLong(arg);
+    }
+
+    inline void toNumber(std::string_view arg, long long & ans) {
+        ans = detail::toLongLong(arg);
+    }
+
+    inline void toNumber(std::string_view arg, unsigned long long & ans) {
+        ans = detail::toUnsignedLongLong(arg);
+    }
+
+    inline void toNumber(std::string_view arg, unsigned char & ans) {
+        ans = detail::toUnsignedChar(arg);
+    }
+
+    inline void toNumber(std::string_view arg, char & ans) {
+        ans = detail::toChar(arg);
+    }
+
+    inline void toNumber(std::string_view arg, signed char & ans) {
+        ans = detail::toSignedChar(arg);
+    }
+
+    inline void toNumber(std::string_view arg, wchar_t & ans) {
+        ans = detail::toWChar_t(arg);
+    }
+
+    inline void toNumber(std::string_view arg, char32_t & ans) {
+        ans = detail::toChar32_t(arg);
+    }
+
+    inline void toNumber(std::string_view arg, char16_t & ans) {
+        ans = detail::toChar16_t(arg);
+    }
+
+    inline std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> toString(char arg) {
+        return detail::fromStringChar(arg);
+    }
+
+    inline std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> toString(unsigned char arg) {
+        return detail::fromStringUnsignedChar(arg);
+    }
+
+    inline std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> toString(signed char arg) {
+        return detail::fromStringSignedChar(arg);
+    }
+
+    inline std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> toString(wchar_t arg) {
+        return detail::fromStringWChar_t(arg);
+    }
+
+    inline std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> toString(char32_t arg) {
+        return detail::fromStringChar32_t(arg);
+    }
+
+    inline std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> toString(char16_t arg) {
+        return detail::fromStringChar16_t(arg);
+    }
+
+    inline std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> toString(int arg) {
+        return detail::fromStringInt(arg);
+    }
+
+    inline std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> toString(unsigned int arg) {
+        return detail::fromStringUnsignedInt(arg);
+    }
+
+    inline std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> toString(short arg) {
+        return detail::fromStringShort(arg);
+    }
+
+    inline std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> toString(unsigned short arg) {
+        return detail::fromStringUnsignedShort(arg);
+    }
+
+    inline std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> toString(long arg) {
+        return detail::fromStringLong(arg);
+    }
+
+    inline std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> toString(long long arg) {
+        return detail::fromStringLongLong(arg);
+    }
+
+    inline std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> toString(unsigned long arg) {
+        return detail::fromStringUnsignedLong(arg);
+    }
+
+    inline std::basic_string<char, std::char_traits<char>, sstd::allocator<char>> toString(unsigned long long arg) {
+        return detail::fromStringUnsignedLongLong(arg);
     }
 
 }/*namespace sstd*/
